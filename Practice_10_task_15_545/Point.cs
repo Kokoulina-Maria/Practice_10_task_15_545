463c38fa43d8f3341c9aae37b0b636fe79efc0d2
Maria <Maria@LAPTOP-N156UH30>
2017-08-30 15:10:06 +0500

    Добавлен класс Point

---
+++ Practice_10_task_15_545/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_10_task_15_545
+{
+    class Point
+    {//класс для элемента списка
+        int info;//информационное поле
+        Point next;//адресное поле
+
+        //свойства
+
+        public int Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+        public Point Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        //конструкторы
+
+        public Point()//конструктор без параметров
+        {
+            info = default(int);
+            next = null;
+        }
+
+        public override string ToString()
+        {
+            return info + " ";
+        }
+    }
+}
