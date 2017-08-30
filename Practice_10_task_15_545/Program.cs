8081352a2870e09285acee16f6bca30ae0fbe296
Maria <Maria@LAPTOP-N156UH30>
2017-08-30 15:20:47 +0500

    Добавлена функция создания исходного списка-круга

--- Practice_10_task_15_545/Program.cs
+++ Practice_10_task_15_545/Program.cs
@@ -8,8 +8,27 @@ namespace Practice_10_task_15_545
 {
     class Program
     {
+        static Point MadeList(int N)
+        {//функция создания исходного списка-круга
+            Point list = new Point();
+            Point beg = new Point();
+            beg = list;
+            for (int i = 1; i <= N; i++)
+            {
+                beg.Info = i;
+                if (i == N) beg.Next = list;
+                else
+                {
+                    beg.Next = new Point();
+                    beg = beg.Next;
+                }
+            }
+            return list;
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
