0a12456bc402915bdb97a286cb75edad1b96c60a
Maria <Maria@LAPTOP-N156UH30>
2017-08-30 16:09:33 +0500

    Программа готова и протестирована

--- Practice_10_task_15_545/Program.cs
+++ Practice_10_task_15_545/Program.cs
@@ -34,9 +34,9 @@ namespace Practice_10_task_15_545
             {
                 for (int i=1; i<=M-1; i++)//считаем до тех пор, пока не дойдем до M-1-ого элемента
                 {
-                    if (i == M-1)//если следующий элемент M-ый
+                    if (i == M-1) //если следующий элемент M-ый
                     {
-                        Console.WriteLine("Участник под номером " + (i+1) + "выбыл");
+                        Console.WriteLine("Участник под номером " + beg.Next.Info + " выбыл");
                         beg.Next = beg.Next.Next;//Удаляем следующий элемент
                     }
                     beg = beg.Next;//передвигаемся на следующий элемент
@@ -44,10 +44,39 @@ namespace Practice_10_task_15_545
             } while (beg.Info != beg.Next.Info);//до тех пор, пока не останется один элемент (следующий за ним не будет равен ему же)
             return beg.Info;
         }
-     
+
+        static int Readint(string msg, string msg1)
+        {// Ввод натурального числа, не равного единице, с проверкой
+            int number; bool ok;// переменная для проверки
+            do
+            {
+                Console.Write(msg);
+                ok = int.TryParse(Console.ReadLine(), out number);
+                if (!ok) Console.WriteLine("Неверный ввод!");
+                if (number <= 1)
+                {
+                    ok = false;
+                    Console.WriteLine(msg1);
+                }
+            } while (!ok);// конец проверки
+            return (number);
+        }
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Считалка");
+            do
+            {
+                int n;//Количество участников
+                int m;//Конечное число считалки
+                n = Readint("Введите количество участников: ", "В круге должно быть больше одного человека!");               
+                m = Readint("Введите m, количество слов в считалке: ", "В считалке должно быть больше одного слова!");
+                Point list = new Point();//список
+                list = MadeList(n);//создаем начальный список
+                int last = FindLast(n, m, list);//ищем последнего человека
+                Console.WriteLine("Номер оставшегося человека: " + last);
+                Console.ReadLine();
+            } while (true);
         }
     }
 }
